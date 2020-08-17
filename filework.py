707a4664d18c142ab9bf818c764babc7bdbdf28d
mradamdouglas <mradamdouglas@gmail.com>
2020-08-17 14:56:06 -0600

    Added final quiz changes and test csv files

--- filework.py
+++ filework.py
@@ -64,22 +64,44 @@ def filestuff():
         else:
             print("\t{} is a directory.".format(item))
 
-#filestuff()
-print("Starting in directory: {}".format(starting_dir))
-os.chdir(starting_dir)
+filestuff()
 
-testdata_fname = 'testdata.csv'
-with open(testdata_fname) as csv_f:
-    csvreader = csv.reader(csv_f)
-    for row in csvreader:
-        name, phone, role = row
-        print("Name: {}, Phone: {}, Role:{}".format(name, phone, role))
+def csvstuff():
+    print("Starting in directory: {}".format(starting_dir))
+    os.chdir(starting_dir)
 
-hosts_fname = 'hosts.csv'
-if not os.path.exists(hosts_fname):
-    hosts =[["workstation.local", "192.168.25.46"], ["webserver.cloud", "10.2.5.6"]]
-    with open(hosts_fname, 'w') as hosts_csv:
-        writer = csv.writer(hosts_fname)
-        writer.writerows(hosts)
-else:
-    os.remove(hosts_fname)
+    testdata_fname = 'testdata.csv'
+    with open(testdata_fname) as csv_f:
+        csvreader = csv.reader(csv_f)
+        for row in csvreader:
+            #Define the 'tuples'
+            name, phone, role = row
+            print("Name: {}, Phone: {}, Role:{}".format(name, phone, role))
+
+    hosts_fname = 'hosts.csv'
+    if not os.path.exists(hosts_fname):
+        hosts =[["workstation.local", "192.168.25.46"], ["webserver.cloud", "10.2.5.6"]]
+        #csv.writer creates a csv record for each list
+        with open(hosts_fname, 'w') as hosts_csv:
+            writer = csv.writer(hosts_csv)
+            writer.writerows(hosts)
+
+    soft_fname = 'software.csv'
+    print("\nReading csv file as dictionary:")
+    with open(soft_fname) as soft_csv:
+        reader = csv.DictReader(soft_csv)
+        for row in reader:
+            print("{} has {} users".format(row["name"], row["users"]))
+
+    dept_fname = 'by_department.csv'
+    if not os.path.exists(dept_fname):
+        users = [{"name": "Sol Mansi", "username": "solm", "department": "IT infrastructure"},
+            {"name": "Lio Nelson", "username": "lion", "department": "User Experience Research"},
+            {"name": "Charlie Grey", "username": "greyc", "department": "development"}]
+        keys = ["name", "username", "department"]
+        #csv.DictWriter creates a csv file from a dictionary with keys as headers
+        with open(dept_fname, 'w') as dept_csv:
+            #identify the keys as the column headers
+            writer = csv.DictWriter(dept_csv, fieldnames=keys)
+            writer.writeheader()
+            writer.writerows(users)
--- quiz.py
+++ quiz.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import datetime
+import csv
 def create_python_script(filename):
     comments = "Start of a new Python program.\n"
     with open(filename, "w") as file:
@@ -42,7 +43,60 @@ def file_date(filename):
     return ("{}".format(str(ti)[:10]))
 
 #print(file_date("newfile.txt"))
-print(os.getcwd())
-dir = os.path.join(os.getcwd(), os.pardir)
-print(os.listdir(dir))
-print(os.path.abspath(dir))
+# print(os.getcwd())
+# dir = os.path.join(os.getcwd(), os.pardir)
+# print(os.listdir(dir))
+# print(os.path.abspath(dir))
+
+# Create a file with data in it
+def create_file(filename):
+  with open(filename, "w") as file:
+    file.write("name,color,type\n")
+    file.write("carnation,pink,annual\n")
+    file.write("daffodil,yellow,perennial\n")
+    file.write("iris,blue,perennial\n")
+    file.write("poinsettia,red,perennial\n")
+    file.write("sunflower,yellow,annual\n")
+
+# Read the file contents and format the information about each row
+def contents_of_file(filename):
+    return_string = ""
+
+    # Call the function to create the file
+    #create_file(filename)
+
+    # Open the file
+    with open(filename) as file:
+    # Read the rows of the file into a dictionary
+        reader = csv.DictReader(file)
+    # Process each item of the dictionary
+        for row in reader:
+            color, name, type = row
+            return_string += "a {} {} is {}\n".format(row["color"], row["name"], row["type"])
+    return return_string
+
+#Call the function
+print(contents_of_file("flowers.csv"))
+
+def skip_header(filename):
+    return_string = ""
+# Open the file
+    with open(filename) as file:
+    # Read the rows of the file
+        rows = csv.reader(file)
+        next(rows)
+    # Process each row
+        for row in rows:
+            one, two, three = row
+        # Format the return string for data rows only
+            return_string += "a {} {} is {}\n".format(one, two, three.strip())
+    return return_string
+
+#Call the function
+#print(skip_header("flowers.csv"))
+
+with open('flowers.csv') as file:
+    lines = csv.reader(file)
+    for line in lines:
+    # lines.sort()
+        print(str(line))
