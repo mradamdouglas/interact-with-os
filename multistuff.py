30f3e9750e7fcf6bfdaf2d49a430ff3ad30745fd
mradamdouglas <mradamdouglas@github.com>
2020-10-01 08:09:12 -0600

    applied final changes

--- multistuff.py
+++ multistuff.py
@@ -6,12 +6,12 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description=
-    "Description: Directory copy with rsync using multiprocessing. \n"
-    "\nBY DEFAULT, the files will only be counted. \nUse optional parameters to list the files and/or perfom rsync copy"
-    "\nDirectory will be copied to a new repository named <repo-name>-backup.\n"
-    "\n           DEFAULT directory is my-sec-repo\n",
-                                formatter_class=argparse.RawDescriptionHelpFormatter,
-                                add_help = True)
+                                 "Description: Directory copy with rsync using multiprocessing. \n"
+                                 "\nBY DEFAULT, the files will only be counted. \nUse optional parameters to list the files and/or perfom rsync copy"
+                                 "\nDirectory will be copied to a new repository named <repo-name>-backup.\n"
+                                 "\n           DEFAULT directory is my-sec-repo\n",
+                                 formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 add_help=True)
 # parser.add_argument(dest='sourceloc', type=str,
 #                     help='Source folder  to be copied')
 #
@@ -29,15 +29,15 @@ parser.add_argument('-v', '--verbose', action='store_true',
 
 parser.add_argument('-s', '--syncargs', dest='syncargs', type=str, default="arq",
                     help='Parameters used for rsynch execution.  Provide a string WITHOUT SPACES OR LEADING DASH. e.g. arq'
-                    'See rsync help for details')
+                         'See rsync help for details')
 
 parser.add_argument('--runsync', dest='runsync', action='store_true',
                     help='Perform the actual rsync operation')
 
 args = parser.parse_args()
 
-_syncargs='-arq'
-_sourceloc='my-sec-repo'
+_syncargs = '-arq'
+_sourceloc = 'my-sec-repo'
 
 if args.override_repo != None:
     _sourceloc = args.override_repo
@@ -47,29 +47,31 @@ if args.syncargs != None:
     _syncargs = "-" + args.syncargs
     print("Rsync arguments provided: {}".format(args.syncargs))
 
-_sourcedir='/home/adouglasx/repos/' + _sourceloc + '/'
+_sourcedir = '/home/adouglasx/repos/' + _sourceloc + '/'
 _backupdir = '/home/adouglasx/repos/' + _sourceloc + '-backup/'
 
 print("Source: {}".format(_sourcedir))
 print("Destination: {}".format(_backupdir))
 print("Rsync args: {}".format(_syncargs))
 
+
 def run_sync(src, sourcedir, backupdir, syncargs):
-        dest = src.replace(sourcedir, backupdir)
-        if args.verbose:
-            print(src)
-        if args.runsync:
-            subprocess.call(["rsync", syncargs, src, dest])
+    dest = src.replace(sourcedir, backupdir)
+    if args.verbose:
+        print(src)
+    if args.runsync:
+        subprocess.call(["rsync", syncargs, src, dest])
+
 
 def create_file_lists(sourcedir):
     sourcelist = []
     for root, dirs, files in os.walk(sourcedir):
         for name in files:
-            sourcefile=(os.path.join(root, name))
+            sourcefile = (os.path.join(root, name))
             sourcelist.append(sourcefile)
         for name in dirs:
-             # sourcelist.append(os.path.join(root, name))
-             pass
+            # sourcelist.append(os.path.join(root, name))
+            pass
     return sourcelist
 
 
@@ -78,15 +80,15 @@ if __name__ == '__main__':
     if os.path.exists(_sourcedir):
         sourcelist = create_file_lists(_sourcedir)
         if len(sourcelist) > 0:
-            print ("Number of files to copy: {}".format(len(sourcelist)))
+            print("Number of files to copy: {}".format(len(sourcelist)))
 
-        '''The argument for pool is the number of processors used for the multiprocessing'''
-            p=Pool(1 if args.processors == None else args.processors)
+            # The argument for pool is the number of processors used for the multiprocessing
+            p = Pool(1 if args.processors == None else args.processors)
 
-        '''functools.partial allows constant parameters to be passed to the Pool
-           Arguments first positional parm is the function to be performed.
-           All other paramaters are arguments for the called method'''
-            parmlist=partial(run_sync, sourcedir=_sourcedir, backupdir=_backupdir, syncargs=_syncargs)
+        # functools.partial allows constant parameters to be passed to the Pool
+        #	Arguments first positional parm is the function to be performed.
+        #	All other paramaters are arguments for the called method
+        parmlist = partial(run_sync, sourcedir=_sourcedir, backupdir=_backupdir, syncargs=_syncargs)
 
-        '''The map method of the pool takes the method to be iterated and the iterator list as arguments'''
-            p.map(parmlist, sourcelist)
+        # The map method of the pool takes the method to be iterated and the iterator list as arguments
+        p.map(parmlist, sourcelist)
